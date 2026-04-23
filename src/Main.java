57535cfa706be451ff04a5ad3123613205744a86
puiusabin <puiumihaisabin@gmail.com>
2026-04-23 15:28:28 +0300

    feat: implement main method

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        RentACarService service = new RentACarService();
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        // Initial data (client + employee)
+        service.addUser(new Client(1, "Ion Popescu", "B123456"));
+        service.addUser(new Employee(2, "Maria Ionescu", 4500.0));
+
+        // Add Vehicles
+        service.addVehicle(new Car(101, "Dacia Logan", 150.0, 5));
+        service.addVehicle(new Van(102, "Ford Transit", 300.0, 15.5));
+        service.addVehicle(new Car(103, "Toyota Yaris", 120.0, 5));
+
+        service.addLocation(new Location(1, "Bucuresti Otopeni"));
+
+        // Testing stdout
+        service.displayAllUsers();
+        service.displayVehiclesSortedByPrice();
+
+        // Create Reservations
+        service.createReservation(1001, 1, 103, 3);
+        service.createReservation(1002, 1, 102, 2);
+
+        // Display all reservations
+        service.displayAllReservations();
+
+        // Find a specific reservation
+        service.displayReservationsByClient(1);
+
+        // Canceling a reservation
+        service.cancelReservation(1002);
+
+        System.out.println("\nDupa anulare:");
+        service.displayAllReservations();
     }
 }

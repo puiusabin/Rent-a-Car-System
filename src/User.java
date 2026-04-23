716d48f46ed2e2664cb305f27bf592a072e96131
puiusabin <puiumihaisabin@gmail.com>
2026-04-23 14:05:53 +0300

    feat: add user class

---
+++ src/User.java
@@ -0,0 +1,14 @@
+abstract class User {
+    protected int id;
+    protected String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public int getId() { return id; }
+    public String getName() { return name; }
+
+    @Override
+    public String toString() { return "User[id=" + id + ", name=" + name + "]"; }
+}

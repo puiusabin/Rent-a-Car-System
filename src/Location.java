692acad34d85ddb65cb7d5496659b2a6a44905fd
puiusabin <puiumihaisabin@gmail.com>
2026-04-23 14:07:37 +0300

    feat: add location class

---
+++ src/Location.java
@@ -0,0 +1,10 @@
+class Location {
+    private int id;
+    private String city;
+
+    public Location(int id, String city) {
+        this.id = id;
+        this.city = city;
+    }
+    public String getCity() { return city; }
+}
